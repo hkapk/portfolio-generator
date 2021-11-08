ce2961e7ec763d713a0e62daf5d7ae33a86a4fff
Hank Kapka <hankkapka@gmail.com>
2021-11-08 12:00:33 -0600

    added feature to prompt user to answer questions and store answers into projects array

--- app.js
+++ app.js
@@ -15,19 +15,49 @@ const promptUser = () => {
     {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a name');
+                return false;
+            }
+        }
     },
 
     {
         type: 'input',
         name: 'github',
-        message: 'What is your Github Username?'
+        message: 'What is your Github Username? (Required)',
+        validate: gitHubInput => {
+            if (gitHubInput) {
+                return true;
+            } else {
+                console.log('Please enter a name');
+                return false;
+            }
+        }
     },
+
+    {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
 
     {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself.'
+        message: 'Provide some information about yourself.',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+              return true;
+            } else {
+              return false;
+            }
+          }
     },
   ]);
 };
@@ -47,12 +77,28 @@ Add a New Project
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('You must enter a project name');
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Enter a description');
+                    return false;
+                }
+            }
           },
           {
             type: 'checkbox',
@@ -63,7 +109,15 @@ Add a New Project
           {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a link');
+                    return false;
+                }
+            }
           },
           {
             type: 'confirm',
